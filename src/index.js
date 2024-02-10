e47bfb4fdd79efb0b5f725b35c7dd1559e124062
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-02-10 22:02:26 +0100

    4.2 Blog list, step2 (extract config)

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
 
-const password = process.argv[2]
-
-const mongoUrl = process.env.MONGO_URI || `mongodb://fullstackopen:${password}@localhost:27017/bloglist`
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
