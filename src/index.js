0aee809a81e36c9c9b365fec53c19687ca5b34dc
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-02-10 09:34:41 +0100

    4.2 Blog list, step2 (extract the controllers)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-const Blog = require('./model/blogs')
+const blogsRouter = require('./controllers/blogs')
 
 const password = process.argv[2]
 
@@ -13,23 +13,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
