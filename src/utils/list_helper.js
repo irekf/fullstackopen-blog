33307f70c9b041ef7761fbec306ea33017b7a024
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-02-11 21:14:50 +0100

    4.4: helper functions and unit tests, step2

--- src/utils/list_helper.js
+++ src/utils/list_helper.js
@@ -4,6 +4,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (!(blogs instanceof Array)) {
+        throw new TypeError('array required')
+    }
+    return blogs.reduce((prev, curr) => prev + curr.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
