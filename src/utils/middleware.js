bf7b65ae5e4fa7e74418de3e4900e1434690d3a6
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-02-24 20:09:49 +0100

    4.8: Blog List Tests, step 1 (add a simpler error handler)

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,4 +1,5 @@
 const morgan = require('morgan')
+const logger = require('./logger')
 morgan.token('post_body', (req) => {
     return JSON.stringify(req.body)
 })
@@ -7,10 +8,15 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
-module.exports.requestLogger = [
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message)
+    next(error)
+}
+
+const requestLogger = [
     morgan('tiny', { skip: (req) => req.method === 'POST' }),
     morgan(':method :url :status :res[content-length] - :response-time ms :post_body',
         { skip: (req) => req.method !== 'POST' })
 ]
 
-module.exports.unknownEndpoint = unknownEndpoint
+module.exports = { requestLogger, unknownEndpoint, errorHandler }
