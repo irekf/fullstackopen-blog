4404ee93538eaf6e56f928990efd845f3c7fda49
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-02-24 21:03:53 +0100

    4.9: Blog List Tests, step 2

--- src/tests/blog_api.test.js
+++ src/tests/blog_api.test.js
@@ -1,14 +1,34 @@
 const mongoose = require('mongoose')
-const { after, test } = require('node:test')
+const { after, before, test } = require('node:test')
+const assert = require('node:assert')
 const supertest = require('supertest')
 const app = require('../app.js')
 
 const api = supertest(app)
+const Blog = require('../model/blogs')
+const { initialBlogs } = require('./test_helper')
 
-test('blogs', async () => {
+before(async () => {
+    await Blog.deleteMany({})
+    const blogs = initialBlogs.map(b => new Blog(b))
+    const promises = blogs.map(b => b.save())
+    await Promise.all(promises)
+})
+
+test('blogs are returned as JSON', async () => {
     await api.get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test('the identification field is id and not _id', async () => {
+    const resultBlogs = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    resultBlogs.body.forEach(b => {
+        assert(b.id)
+        assert.strictEqual(b._id, undefined)
+    })
 })
 
 after(async () => {
