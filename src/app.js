e940d726e6aead15a89496c5a3e42d10b1cba3e8
Irek Fakhrutdinov <irekfakh@gmail.com>
2024-03-14 21:15:55 +0100

    4.18: Blog List Expansion, step 6

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
---
+++ src/controllers/login.js
@@ -0,0 +1,30 @@
+
+const loginRouter = require('express').Router()
+const User = require('../model/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const logger = require('../utils/logger')
+
+loginRouter.post('/', async (request, response) => {
+
+    const { username, password } = request.body
+
+    const user = await User.findOne({ username })
+    logger.info(user)
+    const authenticated = user && await bcrypt.compare(password, user.passwordHash)
+
+    if (!authenticated) {
+        return response.status(401).json({ error: 'username or password incorrect' })
+    }
+
+    const tokenData = {
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(tokenData, process.env.SECRET)
+
+    return response.status(200).json({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
